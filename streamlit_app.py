3f112de1455817301a2300038985fd87691c5150
ChandraBabu <chandrababu.dodda@soulpageit.com>
2025-04-07 12:34:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import streamlit as st
 import pdfplumber
 from src.main import extract_from_document,extract_spec_p_info
 from src.SPEC_P_utils import extract_spec_images
-from pdfminer.pdfparser import PSSyntaxError
+from pdfminer.psparser import PSSyntaxError
 import zipfile
 import re
 st.title('Devfi,Inc')
